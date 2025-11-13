1ab78faf2d7385edd4f58055e96600f27f7177e2
Sagnik-Roy-A <ID+USERNAME@users.noreply.github.com>
2025-11-13 08:17:15 +0530

    Added Workspaces scripts

---
+++ Terraform_Workspaces/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+variable "ami" {
+  description = "This is AMI for the instance"
+}
+
+variable "instance_type" {
+  description = "This is the instance type, for example: t2.micro"
+}
+
+module "ec2_instance" {
+    source = "./modules/EC2_instance"
+    ami = var.ami
+    instance_type = var.instance_type
+}
---
+++ Terraform_Workspaces/terraform.tfvars
@@ -0,0 +1,2 @@
+ami = "ami-02b8269d5e85954ef"
+instance_type = "t2.micro"
